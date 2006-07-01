5afb5b8d88d7afe7699ea575f82ec94661ec5d61
Daniel Silverstone <dsilvers@digital-scurf.org>
2006-07-01 11:34:17 +0100

    When making a dist, we need to get as far away from our top level as we can so that the autoconf/automakery doesn't link into the non-distish tree. This allows config.sub etc to get through properly

--- make-dist.sh
+++ make-dist.sh
@@ -4,19 +4,27 @@ set -ex
 
 TOPPWD=$(pwd)
 
+TOPTEMPDIR=$(mktemp -p $TOPPWD)
+rm $TOPTEMPDIR
+mkdir $TOPTEMPDIR
+
+TEMPDIR=$(mktemp -p $TOPTEMPDIR)
+rm $TEMPDIR
+mkdir $TEMPDIR
+
 cleanup ()
 {
   cd $TOPPWD
-  rm -rf DIST_TREE
+#  rm -rf $TOPTEMPDIR
 }
 
 bzr diff
 
 trap cleanup 0
 
-bzr export DIST_TREE
+bzr export $TEMPDIR/DIST_TREE
 
-cd DIST_TREE
+cd $TEMPDIR/DIST_TREE
 
 autoreconf --install --verbose
 
